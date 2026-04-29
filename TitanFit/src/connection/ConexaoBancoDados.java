757ccaf881d79deddb3a0f272e92145ec56eca1c
mac <mac@192.168.0.253>
2026-04-29 15:07:55 -0300

    configuração inicial do projeto e conexão com banco de dados

--- TitanFit/src/connection/ConexaoBancoDados.java
+++ TitanFit/src/connection/ConexaoBancoDados.java
@@ -1,5 +1,22 @@
 package connection;
 
-public class ConexaoBancoDados {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class ConexaoBancoDados {
+	private static final String URL     = "jdbc:mysql://localhost:3306/titanfit";
+    private static final String USUARIO = "root";
+    private static final String SENHA   = "";
+ 
+    public static Connection conectar() {
+        Connection conexao = null;
+        try {
+            conexao = DriverManager.getConnection(URL, USUARIO, SENHA);
+        } catch (SQLException e) {
+            System.out.println("Erro ao conectar ao banco: " + e.getMessage());
+        }
+        return conexao;
+    }
 }
+
